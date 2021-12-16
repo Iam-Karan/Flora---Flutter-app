439c77a331b9d5851f8e2e2b2364fbdf732d16ec
PriyaGondaliya <priyagondaliya444@gmail.com>
2021-12-15 20:03:38 -0500

    Modify bottomnavigation bar icon

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -103,7 +103,7 @@ class _HomeState extends State<Home> {
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.person,
+              Icons.filter_vintage,
               color: Colors.red,
               size: 40,
             ),
